a0a841fdfd928a474a02bfb2d350b37b79c497b0
Abir Banerjee <abirbanerjee793@gmail.com>
2025-01-24 15:15:15 +0530

    [chore] pip freeze to add dependencies

--- Project0/settings.py
+++ Project0/settings.py
@@ -29,6 +29,25 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 
+CORS_ALLOW_ALL_ORIGINS = True
+
+
+CORS_ALLOWED_METHODS = [
+    'GET',
+    'POST',
+    'PUT',
+    'PATCH',
+    'DELETE',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:5173',  
+    'https://yourfrontenddomain.com',  
+]
+
+
+
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -39,7 +58,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'myapp',
-    'rest_framework'
+    'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -50,6 +70,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',  
 ]
 
 ROOT_URLCONF = 'Project0.urls'
