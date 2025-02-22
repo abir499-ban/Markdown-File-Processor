2e5137e666c071c5c4c7b792f910725621a5964f
Abir Banerjee <abirbanerjee793@gmail.com>
2025-02-22 22:43:13 +0530

    mdParser for bold text added

--- myapp/mdParser.py
+++ myapp/mdParser.py
@@ -3,30 +3,42 @@ import re
 class mdParser:
     def __init__(self):
         self.rules=[
-            (r'^#{1,6}', self.parse_header)
+            (r'^#{1,6}', self.__parse_header),
+            (r'\*\*.*\*\*' , self.__parse_boldText)
         ]
 
         pass
 
     def main(self, text):
         n =len(text)
-        match = re.search(self.rules[0][0] , text)
-        if match:
-            offset = match.end()
-            print("match : " , match.group())
-            print("text: ", text[offset :])
-            print(self.rules[0][1](match.group() , text[offset:]))
-        else:
-            print('No macth')
+        i = 0
+        while True:
+
+            match = re.search(self.rules[i][0] , text)
+            if match:
+                print(self.rules[i][1](match, text))
+                break
+            else:
+                print('No match going for next')
+                i += 1
 
 
-    def parse_header(self, operator, text):
-        return f"<h{len(operator)}>{text} </h{len(operator)}>"
+    def __parse_header(self, match , text):
+        offset = match.end()
+        print("match : " , match.group())
+        print("text: ", text[offset :])
+        return f"<h{len(match.group())}>{text[offset:]} </h{len(match.group())}>"
+    
+    def __parse_boldText(self, match , text):
+        string = match.group()
+        l = len(string)
+        return f"<b>{string[2:l-2]}</b>"
 
 pass
 
-# obj = mdParser()
+obj = mdParser()
+##obj.main('## this is a header')
+##obj.main('** read carefully **')
 
-# obj.m("## This is my Name")
 
 
